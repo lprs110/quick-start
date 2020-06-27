fb3ae2e4acff684bbc9ea2eb402a23854f44df4d
lprs110 <pro.lucasreis@gmail.com>
2020-06-26 21:47:23 -0400

    feat: add app listen

--- nodejs-typescript-simple-setup/src/server.ts
+++ nodejs-typescript-simple-setup/src/server.ts
@@ -5,3 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   return res.json({ message: "Hello World"});
 })
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+})
